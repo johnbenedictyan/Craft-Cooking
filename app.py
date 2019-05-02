be4c8cab7d291efed593c5c792fae79e16a66897
John Benedict <johnyfx00@gmail.com>
2019-05-02 09:56:01 +0000

    User creation feature added

--- app.py
+++ app.py
@@ -2,28 +2,49 @@ from flask import Flask,render_template,request,redirect
 from werkzeug.utils import secure_filename
 import pymongo
 import os
+import pymysql
 from bson import ObjectId
 db_url = "mongodb://dbuser:asd123@cluster0-shard-00-00-6c1o3.mongodb.net:27017,cluster0-shard-00-01-6c1o3.mongodb.net:27017,cluster0-shard-00-02-6c1o3.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
-connection = pymongo.MongoClient(db_url)
-allergens_collection = connection["tgs_codeinstitude_project3_db"]["allergens"]
-authors_collection = connection["tgs_codeinstitude_project3_db"]["authors"]
-posts_collection = connection["tgs_codeinstitude_project3_db"]["posts"]
-recipes_collection = connection["tgs_codeinstitude_project3_db"]["recipes"]
-users_collection = connection["tgs_codeinstitude_project3_db"]["users"]
+mongo_connection = pymongo.MongoClient(db_url)
 
+pymysql_connection = pymysql.connect(host="localhost",
+                             user = "johnbenedict",
+                             password="",
+                             db="craft_cooking")
+                             
 app = Flask(__name__)
 @app.route("/")
 def init():
-    return ("Welcome!")
+    return render_template("index.html")
 
 @app.route("/login")
 def login():
-    return ("Welcome!")
+    return render_template("login.html")
 
 @app.route("/user_creation",methods=["GET","POST"])
 def user_creation():
-    return("Welcome!")
-
+    if request.method == "GET":
+        pymysql_cursor = pymysql.cursors.DictCursor(pymysql_connection)
+        return render_template("signup.html")
+    else:
+        successful_signup = False
+        error = False
+        pymysql_cursor = pymysql.cursors.DictCursor(pymysql_connection)
+        email_input = request.form["email_input"]
+        username_input = request.form["username_input"]
+        password_input = request.form["password_input"]
+        check_if_user_exists_sql = "SELECT * FROM users WHERE username = '{}'".format(username_input)
+        pymysql_cursor.execute(check_if_user_exists_sql)
+        existing_user_check = pymysql_cursor.fetchone()
+        if(existing_user_check==None):
+            sql = "INSERT INTO users (username,password,email) VALUES (\"{}\",\"{}\",\"{}\")".format(username_input,password_input,email_input)
+            pymysql_cursor.execute(sql)
+            successful_signup = True
+        else:
+            error = True
+        pymysql_cursor.close()
+        return render_template("signup.html",successful_signup=successful_signup,error=error)
+        
 @app.route("/recipe_editor/<recipe_id>",methods=["GET","POST"])
 def edit(recipe_id):
     return("Welcome!")
@@ -34,6 +55,10 @@ def recipe_creator():
 
 @app.route("/recipe_deletor/<recipe_id>",methods=["GET","POST"])
 def recipe_deletor():
+    return("Welcome!")
+
+@app.route("/recipe_list")
+def recipe_list():
     return("Welcome!")
     
 if __name__ == '__main__':
---
+++ notes.md
@@ -0,0 +1,10 @@
+{{ url_for('static', filename='')}}
+background-image: url({{ url_for('static', filename='') }})
+
+Things to check how to do:
+1. How to check if select statement returns null 
+
+Things That need to implement:
+1. Login in page, check if the password is correct/username exists.
+2. Change the navigation page when the person is logged in
+3. Recipe editor
