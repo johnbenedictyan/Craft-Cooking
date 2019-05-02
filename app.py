28e9c96f2a322db58e18046729a6d35bffb8b6bb
John Benedict <johnyfx00@gmail.com>
2019-05-02 16:32:28 +0000

    Add login functionality

--- app.py
+++ app.py
@@ -17,9 +17,34 @@ app = Flask(__name__)
 def init():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login",methods=["GET","POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        pymysql_cursor = pymysql.cursors.DictCursor(pymysql_connection)
+        user_does_not_exist = False
+        incorrect_password = False
+        successful_login = False
+        username_input = request.form["username_input"]
+        password_input = request.form["password_input"]
+        check_if_user_exists_sql = "SELECT * FROM users WHERE username = '{}'".format(username_input)
+        check_if_password_is_correct_sql = "SELECT username,id FROM users WHERE username = '{}' AND password= '{}'".format(username_input,password_input)
+        pymysql_cursor.execute(check_if_user_exists_sql)
+        existing_user_check = pymysql_cursor.fetchone()
+        pymysql_cursor.execute(check_if_password_is_correct_sql)
+        correct_password_check = pymysql_cursor.fetchone()
+        if(existing_user_check==None):
+            user_does_not_exist=True
+            return render_template("login.html",user_does_not_exist=user_does_not_exist)
+        elif(correct_password_check==None):
+            incorrect_password=True
+            return render_template("login.html",incorrect_password=incorrect_password)
+        else:
+            successful_login = True
+            user_details=correct_password_check
+            print(user_details)
+            return render_template("index.html",successful_login=successful_login,user_details=user_details)
 
 @app.route("/user_creation",methods=["GET","POST"])
 def user_creation():
@@ -45,20 +70,24 @@ def user_creation():
         pymysql_cursor.close()
         return render_template("signup.html",successful_signup=successful_signup,error=error)
         
-@app.route("/recipe_editor/<recipe_id>",methods=["GET","POST"])
-def edit(recipe_id):
+@app.route("/user/<current_user_id>/recipe_editor/<recipe_id>",methods=["GET","POST"])
+def edit(current_user_id,recipe_id):
     return("Welcome!")
 
-@app.route("/recipe_creator",methods=["GET","POST"])
-def recipe_creator():
+@app.route("/user/<current_user_id>/recipe_creator",methods=["GET","POST"])
+def recipe_creator(current_user_id):
     return("Welcome!")
 
-@app.route("/recipe_deletor/<recipe_id>",methods=["GET","POST"])
-def recipe_deletor():
+@app.route("/user/<current_user_id>/recipe_deletor/<recipe_id>",methods=["GET","POST"])
+def recipe_deletor(current_user_id,recipe_id):
     return("Welcome!")
 
-@app.route("/recipe_list")
-def recipe_list():
+@app.route("/user/<current_user_id>/recipe_list")
+def recipe_list(current_user_id):
+    return("Welcome!")
+    
+@app.route("/user/<current_user_id>")
+def user_dashboard(current_user_id):
     return("Welcome!")
     
 if __name__ == '__main__':
