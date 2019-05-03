326cf22e65d9308dce1706913e8dcfbafec4dd5c
John Benedict <johnyfx00@gmail.com>
2019-05-03 05:41:20 +0000

    Daily Commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template,request,redirect,url_for,session
+from flask_bcrypt import Bcrypt
 from werkzeug.utils import secure_filename
 import pymongo
 import os
@@ -13,8 +14,11 @@ pymysql_connection = pymysql.connect(host="localhost",
                              db="craft_cooking")
                              
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
+bcrypt = Bcrypt(app)
 @app.route("/")
 def init():
+    session['user']='user'
     return render_template("index.html")
 
 @app.route("/login",methods=["GET","POST"])
@@ -28,24 +32,26 @@ def login():
         successful_login = False
         username_input = request.form["username_input"]
         password_input = request.form["password_input"]
-        check_if_user_exists_sql = "SELECT * FROM users WHERE username = '{}'".format(username_input)
-        check_if_password_is_correct_sql = "SELECT username,id FROM users WHERE username = '{}' AND password= '{}'".format(username_input,password_input)
+        check_if_user_exists_sql = "SELECT * FROM users WHERE `username` = '{}'".format(username_input)
+        check_if_password_is_correct_sql = "SELECT username,id FROM users WHERE `username` = '{}' AND `password`= '{}'".format(username_input,password_input)
         pymysql_cursor.execute(check_if_user_exists_sql)
         existing_user_check = pymysql_cursor.fetchone()
         pymysql_cursor.execute(check_if_password_is_correct_sql)
-        correct_password_check = pymysql_cursor.fetchone()
+        user_details = pymysql_cursor.fetchone()
+        pymysql_cursor.close()
         if(existing_user_check==None):
             user_does_not_exist=True
             return render_template("login.html",user_does_not_exist=user_does_not_exist)
-        elif(correct_password_check==None):
+        elif(user_details==None):
             incorrect_password=True
-            return render_template("login.html",incorrect_password=incorrect_password)
+            return render_template("login.html", incorrect_password=incorrect_password)
         else:
             successful_login = True
-            user_details=correct_password_check
-            print(user_details)
             return render_template("index.html",successful_login=successful_login,user_details=user_details)
 
+@app.route("/logout")
+def logout():
+    return redirect(url_for("init"))
 @app.route("/user_creation",methods=["GET","POST"])
 def user_creation():
     if request.method == "GET":
@@ -64,6 +70,7 @@ def user_creation():
         if(existing_user_check==None):
             sql = "INSERT INTO users (username,password,email) VALUES (\"{}\",\"{}\",\"{}\")".format(username_input,password_input,email_input)
             pymysql_cursor.execute(sql)
+            pymysql_connection.commit()
             successful_signup = True
         else:
             error = True
@@ -86,11 +93,45 @@ def recipe_deletor(current_user_id,recipe_id):
 def recipe_list(current_user_id):
     return("Welcome!")
     
-@app.route("/user/<current_user_id>")
+@app.route("/user/<current_user_id>",methods=["GET","POST"])
 def user_dashboard(current_user_id):
-    return("Welcome!")
-    
+    if request.method == "GET":
+        pymysql_cursor = pymysql.cursors.DictCursor(pymysql_connection)
+        user_details_sql="SELECT * FROM users WHERE `id` = '{}'".format(current_user_id)
+        recipe_details_sql="SELECT `recipes`.`id`,`recipes`.`name` FROM recipes LEFT JOIN authors ON `recipes`.`author_id` = `authors`.`id` WHERE `authors`.`user_id` = '{}'".format(current_user_id)
+        pymysql_cursor.execute(user_details_sql)
+        user_details = pymysql_cursor.fetchone()
+        pymysql_cursor.execute(recipe_details_sql)
+        user_recipe_list=pymysql_cursor.fetchall()
+        print(user_recipe_list)
+        pymysql_cursor.close()
+        print(user_details)
+        return render_template("user_dashboard.html",user_details=user_details,user_recipe_list=user_recipe_list)
+    else:
+        pymysql_cursor = pymysql.cursors.DictCursor(pymysql_connection)
+        no_change_required=False
+        password_changed=False
+        email_changed=False
+        email_input = request.form["email_input"]
+        password_input = request.form["password_input"]
+        user_details_sql="SELECT * FROM users WHERE `id` = '{}'".format(current_user_id)
+        pymysql_cursor.execute(user_details_sql)
+        user_details = pymysql_cursor.fetchone()
+        if((email_input==user_details["email"] and password_input==user_details["password"])or(email_input=="" and password_input=="")):
+            no_change_required=True
+        elif((email_input==user_details["email"] or email_input=="") and password_input!=""):
+            password_changed=True
+            update_user_details_sql = "UPDATE users SET password='{}' WHERE id='{}'".format(password_input,current_user_id)
+        else:
+            email_changed=True
+            update_user_details_sql = "UPDATE users SET email = '{}' WHERE id='{}'".format(email_input,current_user_id)
+        pymysql_cursor.execute(update_user_details_sql)
+        pymysql_connection.commit()
+        pymysql_cursor.close()
+        return render_template("login.html",no_change_required=no_change_required,password_changed=password_changed,email_changed=email_changed)
+        
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
+            
--- notes.md
+++ notes.md
@@ -8,3 +8,15 @@ Things That need to implement:
 1. Login in page, check if the password is correct/username exists.
 2. Change the navigation page when the person is logged in
 3. Recipe editor
+4. Find out how to redirect while passing in variables after form post such as login and update user information.
+5. Find out how to implement sessions.
+6. Need to hash the passwords that they enter
+7. Message flashing: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+8. Flask bcrypt: https://flask-bcrypt.readthedocs.io/en/latest/
+9. Flask sessions: https://www.tutorialspoint.com/flask/flask_sessions.htm || https://pythonhosted.org/Flask-Session/
+
+Session:
+app.sercet_key = os.urandom(24)
+
+if 'user' in sessions
+session.pop('user',None)
